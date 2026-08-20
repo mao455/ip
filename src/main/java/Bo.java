7f0a9c742fca0958a819f1f1c2a739c309b99662
mao455 <e1484091@u.nus.edu>
2026-08-20 21:35:18 +0800

    Complete Level-3

--- src/main/java/Bo.java
+++ src/main/java/Bo.java
@@ -21,7 +21,7 @@ public class Bo {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
         while (scanner.hasNextLine()) {
@@ -37,16 +37,79 @@ public class Bo {
             System.out.println(separator);
 
             if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
+            } else if (command.equals("mark") || command.startsWith("mark ")) {
+                markTask(command, tasks, taskCount);
+            } else if (command.equals("unmark") || command.startsWith("unmark ")) {
+                unmarkTask(command, tasks, taskCount);
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println(" added: " + command);
             }
 
             System.out.println(separator);
+        }
+    }
+
+    /**
+     * Marks the task at the index specified by a {@code mark} command as done.
+     *
+     * @param command the complete command entered by the user
+     * @param tasks the tasks in the list
+     * @param taskCount the number of tasks currently stored
+     */
+    private static void markTask(String command, Task[] tasks, int taskCount) {
+        String[] commandParts = command.split("\\s+");
+        if (commandParts.length != 2) {
+            System.out.println(" Invalid mark command. Please specify a task number.");
+            return;
+        }
+
+        try {
+            int taskIndex = Integer.parseInt(commandParts[1]) - 1;
+            if (taskIndex < 0 || taskIndex >= taskCount) {
+                System.out.println(" Invalid task number.");
+                return;
+            }
+
+            tasks[taskIndex].markAsDone();
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   " + tasks[taskIndex]);
+        } catch (NumberFormatException exception) {
+            System.out.println(" Invalid task number.");
+        }
+    }
+
+    /**
+     * Marks the task at the index specified by an {@code unmark} command as not done.
+     *
+     * @param command the complete command entered by the user
+     * @param tasks the tasks in the list
+     * @param taskCount the number of tasks currently stored
+     */
+    private static void unmarkTask(String command, Task[] tasks, int taskCount) {
+        String[] commandParts = command.split("\\s+");
+        if (commandParts.length != 2) {
+            System.out.println(" Invalid unmark command. Please specify a task number.");
+            return;
+        }
+
+        try {
+            int taskIndex = Integer.parseInt(commandParts[1]) - 1;
+            if (taskIndex < 0 || taskIndex >= taskCount) {
+                System.out.println(" Invalid task number.");
+                return;
+            }
+
+            tasks[taskIndex].unmarkAsDone();
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("   " + tasks[taskIndex]);
+        } catch (NumberFormatException exception) {
+            System.out.println(" Invalid task number.");
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,49 @@
+/**
+ * Represents a task tracked by Bo.
+ */
+public class Task {
+    /** The text describing this task. */
+    protected String description;
+
+    /** Whether this task has been marked as done. */
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description the text describing the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /** Marks this task as done. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as not done. */
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the symbol used to display this task's completion status.
+     *
+     * @return {@code X} when done, or a space when not done
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Returns the task in the format used by Bo's task list.
+     *
+     * @return the status icon followed by the task description
+     */
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
