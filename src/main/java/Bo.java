bc5c95ed7952ebe3156194507bbe34c37e396b95
mao455 <e1484091@u.nus.edu>
2026-08-20 20:55:50 +0800

    Complete Level-1

--- src/main/java/Bo.java
+++ src/main/java/Bo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The main entry point for the Bo chatbot.
  */
@@ -15,7 +17,21 @@ public class Bo {
         System.out.println("Hello! I'm Bo.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(separator);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(separator);
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
