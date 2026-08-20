f44d12f7b63c8f165a0e6c68c60448ab1150189a
mao455 <e1484091@u.nus.edu>
2026-08-20 16:23:23 +0800

    Complete Level-0

---
+++ src/main/java/Bo.java
@@ -0,0 +1,21 @@
+/**
+ * The main entry point for the Bo chatbot.
+ */
+public class Bo {
+    public static void main(String[] args) {
+        String banner = " ____        \n"
+                + "| __ )  ___  \n"
+                + "|  _ \\ / _ \\ \n"
+                + "| |_) | (_) |\n"
+                + "|____/ \\___/\n";
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Bo.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
