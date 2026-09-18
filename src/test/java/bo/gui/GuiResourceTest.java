b623565a62ed55812b6ff5827984854a26caebd2
mao455 <e1484091@u.nus.edu>
2026-09-19 01:59:36 +0800

    Group GUI resource assertions

--- src/test/java/bo/gui/GuiResourceTest.java
+++ src/test/java/bo/gui/GuiResourceTest.java
@@ -1,5 +1,6 @@
 package bo.gui;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,9 @@ class GuiResourceTest {
     /** Verifies that the FXML views and stylesheet are available at runtime. */
     @Test
     void guiResources_arePackaged() {
-        assertNotNull(Main.class.getResource("/view/MainWindow.fxml"));
-        assertNotNull(Main.class.getResource("/view/DialogBox.fxml"));
-        assertNotNull(Main.class.getResource("/view/main.css"));
+        assertAll(
+                () -> assertNotNull(Main.class.getResource("/view/MainWindow.fxml")),
+                () -> assertNotNull(Main.class.getResource("/view/DialogBox.fxml")),
+                () -> assertNotNull(Main.class.getResource("/view/main.css")));
     }
 }
